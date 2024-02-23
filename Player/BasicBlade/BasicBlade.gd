7ca4b63aa5d0a4405edd3d99bf456cd9a8b64217
ich <ich@localhost.com>
2024-02-24 00:55:07 +0100

    24.02.2024|00:54

---
+++ Player/BasicBlade/BasicBlade.gd
@@ -0,0 +1,14 @@
+extends Node2D
+@onready var KnifeArea = $"KnifeArea"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func onAttack():
+	KnifeArea.monitorable = true
+	pass##TODO: make the collisionshape rotate to follow the attack animation, and hide the area again
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
