161a361af214678746517e77d5a70ab092fe5be1
songx06 <songximeng061@gmail.com>
2024-02-26 19:23:50 +0100

    animation stuff. and an actual map

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -430.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -22,6 +22,9 @@ var isAttacking = false
 var isFalling = false
 var jumped = false
 var isDoubleJumping = false
+var justFell = false
+var attackAnim = false
+var doubleJumpAnim = false
 
 func _ready():
 	var animation_tree = $"AnimatedSprite2D/AnimationPlayer/AnimationTree"
@@ -33,12 +36,14 @@ func _ready():
 
 func _physics_process(delta):
 	# Add the gravity.
-	
+	justFell = false
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		isFalling = true
 	else:
 		hasDoubleJump = doubleJumpUnlocked
+		if isFalling:
+			justFell = true
 		isFalling = false
 	# Handle jump.[CUSTOMIZED]
 	jumped = false
@@ -47,15 +52,21 @@ func _physics_process(delta):
 		if is_on_floor():
 			velocity.y = JUMP_VELOCITY
 			jumped = true
-		elif doubleJumpUnlocked && hasDoubleJump:
-			velocity.y = JUMP_VELOCITY
+		elif doubleJumpUnlocked && hasDoubleJump && !attackAnim:
+			doubleJumpAnim = true
+			$Timer2.start()
+			isFalling = false
+			velocity.y = JUMP_VELOCITY*1.1
 			isDoubleJumping = true
 			hasDoubleJump= false
 			OnDoubleJump.emit()
 	
 	isAttacking = false
-	if Input.is_action_just_pressed("ui_select") && $"BasicBlade":
+	
+	if Input.is_action_just_pressed("ui_select") && $"BasicBlade" && !doubleJumpAnim:
 		isAttacking = true
+		attackAnim = true
+		$Timer.start()
 		#$"AnimatedSprite2D".animation = "attack"
 		pass
 		
@@ -95,3 +106,11 @@ func _on_animated_sprite_2d_animation_finished():
 	#$"AnimatedSprite2D".animation = "idle"
 	#$"AnimatedSprite2D".play()
 	pass # Replace with function body.
+
+
+func _on_timer_timeout():
+	attackAnim = false
+
+
+func _on_timer_2_timeout():
+	doubleJumpAnim = false
